33be6a7f9105e4d99f33345751e875eb58d0a35c
Ricardo Clarke <wolfpack3371@gmail.com>
2016-06-18 16:36:46 -0400

    modified config/application.rb file

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ Bundler.require(*Rails.groups)
 
 module PokemonComponents
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -32,5 +33,4 @@ module PokemonComponents
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
-  config.assets.initialize_on_precompile = false
 end
