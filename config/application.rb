c66b2c509937e759e4b56e63ff68449e45aa2fab
Ricardo Clarke <wolfpack3371@gmail.com>
2016-06-18 16:47:20 -0400

    assests precompiler

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-def assets_precompiler
-    precompile = config.assets.initialize_on_precompile = false
-return precompile
-end
+config.assets.initialize_on_precompile = false
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
